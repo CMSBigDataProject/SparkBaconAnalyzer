9b6e00a8a4663d719c4c989bd6b6a97ac9976eca
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2016-05-11 21:50:17 -0400

    Plotting only - test (will change)

--- test/SparkAvroStep2.py
+++ test/SparkAvroStep2.py
@@ -1,53 +1,60 @@
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
-#from pyspark.sql.functions import explode
-#from pyspark.sql.functions import udf
-#from pyspark.sql.functions import broadcast
-
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import os
 import sys
+import time
 
-def make_histo(histo,label):
-    ''' Takes histogram in the format of pyspark rdd.histogram
+import numpy as np
+import os
+
+
+def make_histo(label,histos*):
+    ''' Takes histogram as a dictinary output by the histogrammar (variable number)
         and the plot identifier (for instance, pT) and plots the histogram using matplotlib.
         Args:
-            histo: tuple of lists
             label: arbitrary string
+            histos*: histograms to plot
         Output: 
             Output plot in png format. 
     '''
-    bin_edges,data = histo
+    
+    low = histos[0]['data']['low']
+    high = histos[0]['data']['high'] 
+    nbins = len(histos[0]['data']['values'])
+    step = float(high-low)/nbins
+
+    bin_edges = range(low,high,step)
     variable_widths = [(bin_edges[b+1]-bin_edges[b]) for b in range(len(bin_edges)-1)]
 
-    plt.bar(bin_edges[:-1], data, width=variable_widths, color='green')
+    p1 = plt.bar(bin_edges[:-1], histos[0], width=variable_widths, color='b')
+    p2 = plt.bar(bin_edges[:-1], histos[1], width=variable_widths, color='r',bottom= histos[0])
+
     plt.xlabel('Muon '+label)
-    plt.ylabel('Events/bin')
+    plt.ylabel('Entries/bin')
     plt.title('First plots')
     #ax.axis([40, 160, 0, 0.03])
     #ax.grid(True)
-    plt.ylim(0,max(data)*1.5)
+    #plt.ylim(0,max(data1)*5)
+
+    plt.legend((histos[0], histos[1]), (histos[0]['sample'],histos[1]['sample']))
+
     plt.show()
     plt.savefig(label+".png")
 
-
 def main(args):
-    sc = SparkContext(appName="LoadAvroStep2")
-    sqlContext = SQLContext(sc)
+    t0 = time.time()
 
-    selected_muons = sqlContext.read.parquet("/user/alexeys/selected_muons")
-    selected_muons.printSchema()
+    import json
+    #for each data and MC sample read from JSON 
+    #FIXME that will change
+    histo1 = json.load("/user/alexeys/HEPoutput/QCD_HT1000to1500")
+    histo2 = json.load("/user/alexeys/HEPoutput/QCD_HT1500to2000")
+    histo1['sample'] = "QCD_HT1000to1500"
+    histo2['sample'] = "QCD_HT1500to2000"
+    make_histo("pt",*(histo1,histo2))
 
-    #prepare bin edges
-    pt_bin_edges = [10,30,50,80,150,500,100000] 
-
-    #prepare histograms
-    pt_histo = selected_muons.select("pt").rdd.map(lambda x: x.pt).histogram(pt_bin_edges)
-
-    #Back to driver
-    make_histo(pt_histo,"pt")
+    t1 = time.time()
+    print "Elapsed time: ", t1-t0, " sec"
 
 if __name__ == "__main__":
     main(sys.argv)
