a21bf61b725f14cc5185f622772812520d0e58bf
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2016-05-11 23:15:26 -0400

    Multisample stacked plot (placeholder)

--- test/SparkAvroStep2.py
+++ test/SparkAvroStep2.py
@@ -8,7 +8,7 @@ import numpy as np
 import os
 
 
-def make_histo(label,histos*):
+def make_histo(label,*histos):
     ''' Takes histogram as a dictinary output by the histogrammar (variable number)
         and the plot identifier (for instance, pT) and plots the histogram using matplotlib.
         Args:
@@ -21,13 +21,16 @@ def make_histo(label,histos*):
     low = histos[0]['data']['low']
     high = histos[0]['data']['high'] 
     nbins = len(histos[0]['data']['values'])
-    step = float(high-low)/nbins
+    step = (high-low)/nbins
 
-    bin_edges = range(low,high,step)
-    variable_widths = [(bin_edges[b+1]-bin_edges[b]) for b in range(len(bin_edges)-1)]
+    bin_edges = np.linspace(low,high,nbins)
+    #variable_widths = [(bin_edges[b+1]-bin_edges[b]) for b in range(len(bin_edges)-1)]
 
-    p1 = plt.bar(bin_edges[:-1], histos[0], width=variable_widths, color='b')
-    p2 = plt.bar(bin_edges[:-1], histos[1], width=variable_widths, color='r',bottom= histos[0])
+    h1 = histos[0]['data']['values']
+    h2 = histos[1]['data']['values']
+
+    p1 = plt.bar(bin_edges, h1, width=step, color='b')
+    p2 = plt.bar(bin_edges, h2, width=step, color='r',bottom= h1)
 
     plt.xlabel('Muon '+label)
     plt.ylabel('Entries/bin')
@@ -36,7 +39,7 @@ def make_histo(label,histos*):
     #ax.grid(True)
     #plt.ylim(0,max(data1)*5)
 
-    plt.legend((histos[0], histos[1]), (histos[0]['sample'],histos[1]['sample']))
+    plt.legend((p1[0],p2[0]), (histos[0]['sample'],histos[1]['sample']))
 
     plt.show()
     plt.savefig(label+".png")
@@ -47,8 +50,12 @@ def main(args):
     import json
     #for each data and MC sample read from JSON 
     #FIXME that will change
-    histo1 = json.load("/user/alexeys/HEPoutput/QCD_HT1000to1500")
-    histo2 = json.load("/user/alexeys/HEPoutput/QCD_HT1500to2000")
+    with open("/user/alexeys/HEPoutput/QCD_HT1000to1500_13TeV_2_0") as f:
+        histo1 = json.load(f)
+
+    with open("/user/alexeys/HEPoutput/QCD_HT1500to2000_13TeV_2") as g:
+        histo2 = json.load(g)
+
     histo1['sample'] = "QCD_HT1000to1500"
     histo2['sample'] = "QCD_HT1500to2000"
     make_histo("pt",*(histo1,histo2))
